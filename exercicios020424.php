5dd1d2a2195c9afadcc8820a4f8a911eba3a4d9b
Gustavo <67985470+GustavoTrevizani@users.noreply.github.com>
2024-04-08 22:43:41 -0300

    Add files via upload

--- exercicios020424.php
+++ exercicios020424.php
@@ -1,21 +1,44 @@
 <?php
 	// 1. Escreva uma funcao que receba um numero (entre 1 e 10) e escreva a tabuada desse numero.
+function tabuada($n){
+	for($i=1; $i<=10; $i++){
+        $result = $n * $i;    
+        echo("$i x $n = $result <br>");
 
-$i = 5;
-
-function multiplicar($i){
-	$resultado = $i*1;
-
-	echo ("$i x 1 = " . $resultado);
-
-	return $resultado;
-
+	}
 }
-
-
+tabuada(5);
+/*
 // 2. Escreva uma funcao em PHP que recebe um ano como parametro e escreve se este eh um ano bissexto ou nao.
+function bisexto($n){
+    if($n % 4 == 0){
+    	echo("O ano é bisexto");
+    }else{
+    	echo("Não é bisexto");
+    }
+}
+bisexto(2008);
 
 // 3. Modifique o exercicio para encontrar numeros primos (desenvolvido nas aulas anteriores), para que ele utilize funcoes.
+function primo($n){
+   for($nu=0; $nu<=$n; $nu++){
+	    $ehPrimo = true;
+    for ($j=2; $j<$nu; $j++){
+       if($nu % $j == 0){
+            $ehPrimo = false;
+		}
+    }
+        if ($ehPrimo == true){
+            echo("$nu ");
+        }
 
-// 4. Crie uma funcao que receba uma string como entrada e retorne o numeor de palavras dessa string.
+    }
+ }
+ primo(30);
+
+// 4. Crie uma funcao que receba uma string como entrada e retorne o numero de palavras dessa string.
+$ola = ("ola mun do ge n te");
+
+echo(count(explode(" ",$ola)));
+
 ?>
